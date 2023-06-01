a2e4d3d294d756894b8a5b4b49774eed8a7999e0
smyths-kesavarao <kkovurunaga@smythstoys.com>
2023-06-01 09:45:01 +0530

    Project Setup

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1 class="text-center p-5 text-3xl text-blue-500">
+    <a href="https://www.smythstoys.com/" target="_blank"
+      >https://www.smythstoys.com/</a
+    >
+  </h1>
+</template>
